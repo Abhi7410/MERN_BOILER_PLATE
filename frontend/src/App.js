5fd73dc536d1e276f6f77a9e288ae88aca1363ab
abhi7410 <abhihsr1414@gmail.com>
2022-08-28 17:43:17 +0530

    Login and Register Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Dashboard  from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello world</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+     </Router>
+    </>
   );
 }
 
